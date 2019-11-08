cbda0d57e19b8949baff87085a632e813bc965cf
Quantum Philosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-11-07 21:54:44 -0800

    adding localhost

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import AppNavigator from './navigation/AppNavigator';
 import io from 'socket.io-client'
 
 // const socket = io('https://blobber-native.herokuapp.com/')
-const socket = io('http://10.0.2.2:3030')
+// const socket = io('http://10.0.2.2:3030')
+const socket = io('http://localhost:3030')
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
